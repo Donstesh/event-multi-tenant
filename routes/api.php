351fb4ffa68bcb16b1ea2f032683028833b00256
Donstesh <stephenshifoko@gmail.com>
2025-05-19 16:36:03 +0300

    api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\EventController;
+use App\Http\Controllers\Admin\AttendeeController;
+use App\Http\Controllers\PublicEventController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('{org}')->group(function () {
+    Route::get('/events', [PublicEventController::class, 'index']);
+    Route::post('/register', [PublicEventController::class, 'register']);
+});
+
+// Admin routes (protected)
+Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
+    Route::apiResource('events', EventController::class);
+    Route::get('attendees', [AttendeeController::class, 'index']);
+});
+
+// Public routes
+Route::prefix('{org}')->group(function () {
+    Route::get('events', [PublicEventController::class, 'index']);
+    Route::post('register', [PublicEventController::class, 'register']);
+});
+
